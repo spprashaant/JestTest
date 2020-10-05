22bce4e6f211ad6d32af8865517ff3f82a2a581a
Prashanth Padebettu <prashanth.padebettu@gds.ey.com>
2020-10-05 21:52:28 +0530

    first draft

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const [error, setError] = React.useState(false);
+
   return (
-    <div className="App">
-      <h1>Learn React Testing</h1>
+    <div data-test="component-app">
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">{count}</span>
+        <span id="error-message" className={`error ${error ? '': 'hidden'}`} data-test="error-message">Counter cannot be less than zero</span>
+
+      </h1>
+      <button 
+        onClick={() => {
+          if(error) { setError(false);}
+          setCount(count + 1)
+        }
+          }
+        data-test="increment-button">Increment counter</button>
+        <button
+          onClick={() => {
+            if(count > 0){
+              setCount(count - 1);
+              
+            } else {
+              setError(true);
+            }
+          }
+        } data-test="decrement-button">Decrement counter</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,90 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({adapter: new EnzymeAdapter() });
+
+
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper,'component-app');
+  expect(appComponent.length).toBe(1);
 });
+
+test("renders button", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper,'increment-button');
+  expect(button.length).toBe(1);
+});
+
+test("renders decrement button", () => {
+  const wrapper = setup();
+  const decrementButton = findByTestAttr(wrapper, 'decrement-button');
+  expect(decrementButton.length).toBe(1);
+})
+
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper,'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+});
+
+test("clicking on button increments counter", () => {
+  const wrapper = setup();
+
+  const button = findByTestAttr(wrapper, "increment-button");
+
+  button.simulate('click');
+
+  const count = findByTestAttr(wrapper, "count").text();
+
+  expect(count).toBe("1");
+});
+
+test("clicking on decrement button increments counter", () => {
+  const wrapper = setup();
+
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate('click');
+  const decrementButton = findByTestAttr(wrapper, "decrement-button");
+
+  decrementButton.simulate('click');
+
+  const count = findByTestAttr(wrapper, "count").text();
+
+  expect(count).toBe("0");
+});
+
+test("counter is 0 and decrement is clicked", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "decrement-button");
+  button.simulate('click');
+
+  const errorDiv = findByTestAttr(wrapper, "error-message");
+  const errorHasHiddenClass = errorDiv.hasClass('hidden');
+  expect(errorHasHiddenClass).toBe(false);
+});
+
+test("clicking the increment clears the error", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "decrement-button");
+  button.simulate('click');
+  const incButton = findByTestAttr(wrapper, "increment-button");
+  incButton.simulate('click');
+
+  const errorDiv = findByTestAttr(wrapper, "error-message");
+  const errorHasHiddenClass = errorDiv.hasClass('hidden');
+  expect(errorHasHiddenClass).toBe(true);
+
+})
